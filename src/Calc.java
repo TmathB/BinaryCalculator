82891627ebd0ba790942cd199ce06d54bc65ed03
Mat <matheus_barbosa@usp.br>
2019-04-05 13:28:34 -0300

    Sum method add

--- src/Calc.java
+++ src/Calc.java
@@ -1,116 +1,176 @@
-import java.io.PrintStream;
 import java.util.Scanner;
+import java.util.Arrays; 
 
-public class Calc
-{
-  public Calc() {}
-  
-  static int[] sum(int[] paramArrayOfInt) {
-    int i = paramArrayOfInt[0];
-    int j = paramArrayOfInt[1];
-    
-    int[] arrayOfInt = new int[2];
-    
+public class Calc {
+	
+	 static int [] sum(int [] packA,int [] packB ){
+		
 
-    if ((i == 1) && (j == 1)) {
-      arrayOfInt[0] = 0;
-      arrayOfInt[1] = 1;
-    }
-    else if ((i == 1) && (j == 0)) {
-      arrayOfInt[0] = 1;
-      arrayOfInt[0] = 0;
-    }
-    else if ((i == 0) && (j == 1)) {
-      arrayOfInt[0] = 1;
-      arrayOfInt[1] = 0;
-    }
-    else if ((i == 0) && (j == 0)) {
-      arrayOfInt[0] = 0;
-      arrayOfInt[1] = 0;
-    }
-    
-    return arrayOfInt;
-  }
-  
-  public static void main(String[] paramArrayOfString) { java.util.Scanner localScanner = new java.util.Scanner(System.in);
-    
+		int [] computed = new int [packA.length+1];
+		int [] carryOut = new int [packA.length+1];
+		int a ;
+		int b ; 
+		
+		
+		
+		for(int i = packA.length-1 ;i>=0;i--) {
+			
+			
+			a = packA[i];
+			b = packB[i];
+
+	
+			if(carryOut[i] == 0 && a == 0 && b ==0 ) {
+				computed[i+1] = 0;
+				carryOut[i] = 0;// Carry Out
+			}
+			else if(carryOut[i] == 0 && a == 0 && b ==1 ) {
+				computed[i+1] = 1;
+				carryOut[i] = 0;// Carry Out
+			}
+			else if(carryOut[i] == 0 && a == 1 && b ==0 ) {
+				computed[i+1] = 1;
+				carryOut[i] = 0;// Carry Out
+			}
+			else if(carryOut[i] == 0 && a ==1 && b ==1 ) {
+				computed[i+1] = 0;
+				carryOut[i] = 1;// Carry Out
+			}
+			else if(carryOut[i] == 1 && a == 0 && b ==0 ) {
+				System.out.println(i);
+				computed[i+1] = 1;
+				carryOut[i] = 0;// Carry Out
+			}
+			else if(carryOut[i] == 1 && a == 0 && b ==1 ) {
+				computed[i+1] = 0;
+				carryOut[i+2] = 1;// Carry Out
+			}
+			else if(carryOut[i] == 1 && a == 1 && b ==0 ) {
+				computed[i+1] = 0;
+				carryOut[i] = 1;// Carry Out
+			}
+			else if(carryOut[i] == 1 && a == 1 && b ==1 ) {
+				computed[i+1] = 1;
+				carryOut[i] = 1;// Carry Out
+			}
+		}
+		System.out.println();
+		for (int i = 0 ; i < carryOut.length ; i++)
+			System.out.print("   "+carryOut[i]);/**/
+
+		return computed;
+	}
+	public static void main(String[]args){
+		
+		//Entrada do usuario
+		Scanner sc1 = new Scanner(System.in); 
+    	
+		//System.out.println("Digite o sinal do numero A");
+		//String signalA = sc1.next()
+    	System.out.println("Digite um valor");
+    	String valueA = sc1.next();
+    	
+    	//System.out.println("Digite o sinal do numero B");
+    	//String signalB = sc1.next();
+    	System.out.println("Digite um valor");
+    	String valueB = sc1.next();
+    	
+    	int bigest = Math.max(valueA.length(),valueB.length());
+    	
+    	int [] packA = new int[bigest];
+    	int [] packB = new int[bigest];
+ 	
+    	System.out.println();
+    	
+    	//Conversão e alinhamento dos imputs de entrada
+    	
+    	int j = 0;
+    	if(valueA.length() == valueB.length()) {
+    		for(int i = 0 ;i<=packA.length-1;i++) {
+        		
+    			packA[i] = Character.getNumericValue(valueA.charAt(i));
+        				
+        		  	 
+        	}
+	    	for(int i = 0  ;i <=valueA.length()-1  ;i++) {	
+	    		
+	    		
+	    	    packB[i] = Character.getNumericValue(valueB.charAt(i));
+	    						
+	    		
+	    	}
+    		
+    	}
+    	else if(valueA.length()>valueB.length()) {
+    		for(int i = 0 ;i<bigest;i++) {
+        		try {
+        			packA[i] = Character.getNumericValue(valueA.charAt(i));
+        		}catch(Exception e){}       	 
+        	}
+	    	for(int i = valueA.length()-valueB.length() ;i <bigest ;i++) {	
+	    		
+	    		try {
+	    			packB[i] = Character.getNumericValue(valueB.charAt(j));
+	    			j++;			
+	    		}catch(Exception e){}
+	    		
+	    		//System.out.print(i);
+	    	}
+    	}else {
+    		for(int i = valueB.length()-valueA.length() ;i<bigest;i++) {
+        		try {
+        			packA[i] = Character.getNumericValue(valueA.charAt(j));
+        			j++;
+        		}catch(Exception e){}        	  
+        	}
+    		for(int i = 0 ;i <bigest ;i++) {	
+		    		
+		    	try {
+		    		packB[i] = Character.getNumericValue(valueB.charAt(j));
+		    		j++;			
+		    	}catch(Exception e){}		    
+		    }
+    	}
+    	
+    	//Passagem para respectiva função
+    	
+    	//pack [0] = Integer.parseInt(valueA) ;
+    	//pack [1] = Integer.parseInt(valueB) ;
+    	//pack [2] = Integer.parseInt(signalA);
+    	//pack [3] = Integer.parseInt(signalB);
 
 
-    System.out.println("Digite um valor");
-    String str1 = localScanner.next();
-    
+		/**/
+    	
+    	/**/
+    	System.out.println();
+    	System.out.print("    ");
+    	for(int i = 0 ;i <bigest ;i++) {	
+    		System.out.print("   "+packA[i]);
+    	}
+    	System.out.println();
+    	System.out.print("    ");
+    	for(int i = 0 ;i <bigest ;i++) {
+    		System.out.print("   "+packB[i]);
+    	};
+    	System.out.println();
+    	System.out.println ("....................................");
+    	
 
+    	System.out.println();
+		for (int i = 0 ; i < result.length ; i++)
+			System.out.print("   "+result[i]);/**/
+    	
+    	
+		//System.out.println(valueA+" + "+valueB+" = "+result[0]+" | Carry out  = "+result[1]);
+    	
+    	
+    	
+    	
 
-    System.out.println("Digite um valor");
-    String str2 = localScanner.next();
-    
-    int i = Math.max(str1.length(), str2.length());
-    
-    int[] arrayOfInt1 = new int[i];
-    int[] arrayOfInt2 = new int[i];
-    
+    	
 
+	}
+	
 
-
-
-
-
-
-
-
-
-    System.out.println();
-    int j = 0;
-    if (str1.length() > str2.length()) {
-      for (k = 0; k < i; k++) {
-        try {
-          arrayOfInt1[k] = Character.getNumericValue(str1.charAt(k));
-        }
-        catch (Exception localException1) {}
-      }
-      
-
-      for (k = str1.length() - str2.length(); k < i; k++)
-      {
-        try {
-          arrayOfInt2[k] = Character.getNumericValue(str2.charAt(j));
-          j++;
-        }
-        catch (Exception localException2) {}
-        System.out.print(k);
-      }
-    } else {
-      for (k = str2.length() - str1.length(); k < i; k++) {
-        try {
-          arrayOfInt1[k] = Character.getNumericValue(str1.charAt(j));
-          j++;
-        }
-        catch (Exception localException3) {}
-      }
-      
-
-      for (k = 0; k < i; k++)
-      {
-        try {
-          arrayOfInt2[k] = Character.getNumericValue(str2.charAt(j));
-          j++;
-        }
-        catch (Exception localException4) {}
-        System.out.print(k);
-      }
-    }
-    
-
-    System.out.println();
-    for (int k = 0; k < i; k++) {
-      System.out.print(arrayOfInt1[k] + "  ");
-    }
-    System.out.println();
-    for (k = 0; k < i; k++) {
-      System.out.print(arrayOfInt2[k] + "  ");
-    }
-    
-    System.out.println();
-    System.out.println("----------------------------------------");
-  }
 }
