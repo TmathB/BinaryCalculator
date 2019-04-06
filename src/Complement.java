4d523ed387468c2f9704278fd5745a62758c96a3
Jyun <gustavohayashida00@gmail.com>
2019-04-06 19:09:42 -0300

    Complemento atualizado

--- src/Complement.java
+++ src/Complement.java
@@ -3,28 +3,29 @@ public class Complement {
  public Complement() {}
  
  
- 
- public static int [] um = {1};
- 
  public static int [] doComplement(int[] packA) {
-     
+	 
+	int [] Um = new int [packA.length];
+	
+	Um [packA.length-1] = 1;
+   
+   
 	Sum soma = new Sum();
 	
-	 for(int i = 0 ; i < packA.length ; i++){
+	for(int i = 0 ; i < packA.length ; i++){
+		 
 		 if(packA[i] == 0){
-			 packA[i] = 1;
+			packA[i] = 1;
 		 }
 	     else if(packA[i] == 1){
-			 packA[i] = 0;
+			packA[i] = 0;
 		 }
-		 soma.doSum(packA,um);
-				 
-	 }
-	 System.out.print(packA);
+	}
 	 
-	 return packA;
+	soma.doSum(packA,Um);
+				 
+	return packA;
 	 
  }
-
-
+ 
 }
