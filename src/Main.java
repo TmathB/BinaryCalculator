d3c2f44cf419b43c23f5dc452f428914b89c0f7a
Jyun <gustavohayashida00@gmail.com>
2019-05-02 08:18:57 -0300

    Main atualizado

--- src/Main.java
+++ src/Main.java
@@ -11,7 +11,7 @@ import java.util.Scanner;
 //	Prof. João Luiz Bernardes Junior									  //
 //************************************************************************//	
 
-/**O usuario deve compilar essa classe para entrada e saida das operacoes**/
+/**O usuario deve compilar essa classe para execucao do programa**/
 
 
 /**=====================================================================================================================================================**/	
@@ -28,29 +28,102 @@ public class Main {
 		
 		
 
-        System.out.println("// Digite o numero correspondente (1)Int (2)Float.");
+        System.out.println("// Digite o tipo de  numero binario correspondente (1)Int (2)Float.");
         String kindOfbin = sc1.next();
+		
+		if(!kindOfbin.equals("1") && !kindOfbin.equals("2")){
+			System.out.println();
+			System.out.println("// Apenas sao aceitos os numeros 1 ou 2 para realizacao da conta!");
+			System.out.println();
+			System.out.println("// Encerrando programa...");
+			System.out.println();
+			System.out.println("<------------------------------------------------------------------------------------------------------------------------------------------>");
+			
+			System.exit(0);
+			
+		}
 
         System.out.println("// Digite a operacao '+', '-', 'x' ou '/'. ");
         String operation = sc1.next();
+		
+		
+		if(!operation.equals("+") && !operation.equals("-") && !operation.equals("x") && !operation.equals("/")){
+			System.out.println();
+			System.out.println("// Sao aceitas apenas as 4 operacoes acima!");
+			System.out.println();
+			System.out.println("// Encerrando programa...");
+			System.out.println();
+			System.out.println("<------------------------------------------------------------------------------------------------------------------------------------------>");
 
+			System.exit(0);
+			
+		}
+		
         System.out.println("// Digite o tamanho bits (Sem contar o bit de sinal).");
         String bitSize = sc1.next();
-
+		
         System.out.println("// Digite o bit de sinal do 1 numero.");
         String signalA = sc1.next();
+		
+		if(!signalA.equals("0") && !signalA.equals("1")){
+			System.out.println();
+			System.out.println("// Escreva um bit de sinal valido!");
+			System.out.println();
+			System.out.println("// Encerrando programa...");
+			System.out.println();
+			System.out.println("<------------------------------------------------------------------------------------------------------------------------------------------>");
+
+			System.exit(0);
+			
+		}
+
 
         System.out.println("// Digite o valor em binario do 1 numero.");
         String valueA = sc1.next();
+		
+		if(!valueA.matches("[0-1]+")){
+			System.out.println();
+			System.out.println("// Escreva um numero binario valido!");
+			System.out.println();
+			System.out.println("// Encerrando programa...");
+			System.out.println();
+			System.out.println("<------------------------------------------------------------------------------------------------------------------------------------------>");
+
+			System.exit(0);
+			
+		}
 
         System.out.println("// Digite o bit de sinal do 2 numero.");
         String signalB = sc1.next();
+		
+		if(!signalB.equals("0") && !signalB.equals("1")){
+			System.out.println();
+			System.out.println("// Escreva um bit de sinal valido!");
+			System.out.println();
+			System.out.println("// Encerrando programa...");
+			System.out.println();
+			System.out.println("<------------------------------------------------------------------------------------------------------------------------------------------>");
+
+			System.exit(0);
+			
+		}
 
         System.out.println("// Digite valor em binario do 2 numero.");
         String valueB = sc1.next();
+		
+			if(!valueB.matches("[0-1]+")){
+			System.out.println();
+			System.out.println("// Escreva um numero binario!");
+			System.out.println();
+			System.out.println("// Encerrando programa...");
+			System.out.println();
+			System.out.println("<------------------------------------------------------------------------------------------------------------------------------------------>");
+
+			System.exit(0);
+			
+		}
 
         if(kindOfbin.equals("1")) {
-            //System.out.println("Fazendo conta int");
            SelectPrint selP = new SelectPrint();
            SelectPrint.select(valueA,valueB,signalA,signalB,operation,bitSize);
 
@@ -66,4 +139,6 @@ public class Main {
 
     }
 }
+
+
 
